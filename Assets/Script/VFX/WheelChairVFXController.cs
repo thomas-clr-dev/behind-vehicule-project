ee17f8ac1c9fba09d79cd4055123992925f499b7
Antoine <>
2026-05-04 14:34:35 +0200

    fix (no clean) Script for fx wheelchair

--- Assets/Script/VFX/WheelChairVFXController.cs
+++ Assets/Script/VFX/WheelChairVFXController.cs
@@ -2,12 +2,28 @@
 
 using UnityEngine;
 
+
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+//using System.Numerics;
+
+
 public class WheelVFX : MonoBehaviour
 
 {
+
+    public Vector3 normalized;
+
+
+    public List<AudioClip> wheelList;
+    public AudioSource audioWheel;
+
     private bool vfxAlreadyPlayed = false;
 
     private EventBinding<WheelStateDataEvent> dataBinding;
+    
 
     [SerializeField] private GameObject vfxPrefab;
     private void OnEnable()
@@ -19,16 +35,29 @@ public class WheelVFX : MonoBehaviour
 
     private void OnDataUpdated(WheelStateDataEvent e)
     {
-        Debug.Log(e.MotorTorque);
-        if(e.MotorTorque > 5 && !vfxAlreadyPlayed)
+        //Debug.Log(e.MotorTorque);
+        if(e.MotorTorque == 97  && !vfxAlreadyPlayed) 
         {
+            Debug.Log("Je me lance + " +e.MotorTorque);
+
             GameObject go = Instantiate(vfxPrefab, transform.position, transform.rotation);
 
             vfxAlreadyPlayed = true;
+
+            if(wheelList != null)
+            {
+                int r =Random.Range(0, wheelList.Count);
+                audioWheel.clip = wheelList[r];
+                audioWheel.PlayOneShot(wheelList[r], 0.5f);
+            } 
+
+
+            
         }
 
         else if (e.MotorTorque <1)
         {
+            Debug.Log("Reset bool");
             vfxAlreadyPlayed = false;
         }
     }
