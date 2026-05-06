90d3852d1ac58514e0992a0ec66dbf4732efacfd
Abdou Mouaad <shligom.bongonb34@gmail.com>
2026-05-06 13:47:15 +0200

    Move prefab folder

---
+++ Assets/Script/Core/Camera/CinemachineCameraController.cs
@@ -0,0 +1,74 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+[RequireComponent(typeof(CinemachineCamera))]
+public class CinemachineCameraController : MonoBehaviour, EventListener<CameraEvent>
+{
+    [SerializeField] private Flag cameraFlag;
+    private int basePriority = 10;
+    public WheelChairController TargetCharacter { get; set; }
+
+    private CinemachineCamera virtualCamera;
+
+    private void Awake()
+    {
+        virtualCamera = GetComponent<CinemachineCamera>();
+    }
+    public void OnEvent(CameraEvent cameraEvent)
+    {
+        switch (cameraEvent.EventType)
+        {
+            case CameraEventTypes.ActivateCamera:
+                virtualCamera.Priority = (cameraEvent.CameraFlag == cameraFlag) ? 20 : 10;
+                break;
+            case CameraEventTypes.SetTargetCharacter:
+                if (cameraEvent.TargetCharacter != null)
+                {
+                    SetTarget(cameraEvent.TargetCharacter);
+                }
+                break;
+            case CameraEventTypes.SetConfiner:
+
+                break;
+            case CameraEventTypes.StartFollowing:
+                StartFollowing();
+                break;
+            case CameraEventTypes.StopFollowing:
+
+                break;
+            case CameraEventTypes.RefreshPosition:
+
+                break;
+            case CameraEventTypes.ResetPriorities:
+
+                break;
+            case CameraEventTypes.RefreshAutoFocus:
+
+                break;
+            case CameraEventTypes.RefreshRotation:
+
+                break;
+        }
+    }
+
+    public void StartFollowing()
+    {
+        virtualCamera.Follow = TargetCharacter.transform;
+    }
+
+    public void SetTarget(WheelChairController targetCharacter)
+    {
+        TargetCharacter = targetCharacter;
+    }
+
+    private void OnEnable()
+    {
+        this.EventStartListening<CameraEvent>();
+    }
+
+    private void OnDisable()
+    {
+        this.EventStopListening<CameraEvent>();
+    }
+}
---
+++ Assets/Script/Core/ScriptableObjects/Flag.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Flag", menuName = "ScriptableObjects/Flag", order = 1)]
+public class Flag : ScriptableObject
+{
+   
+}
