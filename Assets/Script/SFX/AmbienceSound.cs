f48c95a80b4a58d7417e0a0973ef6ee15ad2c2b2
Antoine <>
2026-05-06 13:58:27 +0200

    Add Ambiance script totally custom for each ambiances

---
+++ Assets/Script/SFX/AmbienceSound.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public class AmbienceSound : MonoBehaviour
+{
+    public Collider Area;
+    public GameObject Player;
+
+    public List<AudioClip> ambList;
+    public AudioSource ambSource;
+
+    public float timeSound = 5;
+ 
+    void Start()
+    {
+        if(!ambSource.isPlaying)
+        {
+            PlayAmb();
+        }
+    }
+
+    void Update()
+    {
+        Vector3 closestPoint = Area.ClosestPoint(Player.transform.position);
+
+        transform.position = closestPoint;
+
+        /*if(!ambSource.isPlaying)
+        {
+            PlayAmb();
+        }*/
+    }
+
+    void PlayAmb()
+    {
+        if(!ambSource.isPlaying)
+        {
+            int r = Random.Range(0, ambList.Count);
+            AudioClip clip = ambList[r];
+            ambSource.clip = clip;
+            ambSource.Play();
+            ambSource.panStereo = Random.Range(-0.9f, 0.9f);
+            StartCoroutine(AmbPlay());
+            Debug.Log("je fais du bruit");
+        }
+        
+    }
+
+    IEnumerator AmbPlay()
+    {
+        yield return new WaitForSeconds(timeSound);
+        ambSource.Stop();
+        PlayAmb();
+    }
+
+}
