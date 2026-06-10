a0fa57f653ebe47e1ecfcab89e03fe878f00c59b
Linkdu <chaillouxmarius@gmail.com>
2026-06-10 14:52:10 +0200

    subtitles + TUTORIAL

--- Assets/BEHIND/Script/Core/Camera/CameraZoneTrigger.cs
+++ Assets/BEHIND/Script/Core/Camera/CameraZoneTrigger.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CameraZoneTrigger : MonoBehaviour
 {
     [SerializeField] private Flag cameraToActivate;
     [SerializeField] private bool destroyAfterUse = true;
 
+    public UnityEvent Event;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             CameraEvent.Trigger(CameraEventTypes.ActivateCamera, null, null, null, cameraToActivate);
+
+            Event?.Invoke();
 
             if (destroyAfterUse) Destroy(gameObject);
         }
