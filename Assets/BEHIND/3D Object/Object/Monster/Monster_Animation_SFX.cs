07c7f69d526db0898d5e81bfddd02b10b9b76e59
Antoine <>
2026-06-10 14:39:48 +0200

    Add all Monster SFX Animation (Scream - Growl - Footsteps)

---
+++ Assets/BEHIND/3D Object/Object/Monster/Monster_Animation_SFX.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Monster_Animation_SFX : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public List<AudioClip> Footsteps;
+    public List<AudioClip> Screams;
+    public List<AudioClip> Growls;
+
+    public void Footstep_Monster()
+    {
+        int r = UnityEngine.Random.Range(0, Footsteps.Count);
+        audioSource.PlayOneShot(Footsteps[r]);
+    }
+
+    public void Scream_Monster()
+    {
+        int r = UnityEngine.Random.Range(0, Screams.Count);
+        audioSource.PlayOneShot(Screams[r]);
+
+        int rr = UnityEngine.Random.Range(0, Growls.Count);
+        audioSource.PlayOneShot(Growls[r]);
+    }
+
+    
+}
