a3cfcf69b1e42dd44af70a93de0cf3c6cc387e77
Antoine <>
2026-06-11 17:08:39 +0200

    Add WheelGrab VFX

--- Assets/BEHIND/R&D/R&D Player/Scripts/WheelChair/WheelChairSFX.cs
+++ Assets/BEHIND/R&D/R&D Player/Scripts/WheelChair/WheelChairSFX.cs
@@ -8,7 +8,13 @@ public class WheelChairSFX : MonoBehaviour, IEventListener<PushEvent>
 
     public List<AudioClip> grabList;
     public AudioSource audioGrab;
-       
+    public GameObject vfxPrefab;
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    public float cooldownTime = 2f;
+    private bool vfxAlreadyPlayed = false;
+    private float lastPlayedTime = -2f;
 
       private void Start()
     {
@@ -23,7 +29,14 @@ public class WheelChairSFX : MonoBehaviour, IEventListener<PushEvent>
             {
                 int r = Random.Range(0, grabList.Count);
                 audioGrab.PlayOneShot(grabList[r], 0.5f);
+
             }
+        if (!vfxAlreadyPlayed && Time.time >= lastPlayedTime + cooldownTime)
+        {
+            Instantiate(vfxPrefab, leftHand.transform.position, leftHand.transform.rotation);
+            Instantiate(vfxPrefab, rightHand.transform.position, rightHand.transform.rotation); 
+        }
+            
     }
 
     private void OnDisable()
